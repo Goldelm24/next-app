7da5e1d13bd4c7f621e3c78f92ba76fdf77874d2
Josias Guzman <jguzman@aoiths.org>
2023-10-07 11:58:40 -0400

    Learning Nextjs

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,10 +1,12 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const NewUserPage = () => {
+const router = useRouter();
+
   return (
-    <div>
-      Klk mami chula
-    </div>
+    <button className='btn btn-primary' onClick={() => router.push('/users')}>Create</button>
   )
 }
 
